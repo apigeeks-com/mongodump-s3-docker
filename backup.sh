cd473d2f31da32b918ddf564d8db46794aff6923
Vitalii Trachenko <vitaltrachenko@gmail.com>
2019-03-21 16:49:15 +0200

    feat (core): add aws s3 bucket support

--- backup.sh
+++ backup.sh
@@ -3,9 +3,11 @@ set -e
 BACKUP_NAME="mongodump_$(date -u +%Y-%m-%d_%H-%M-%S).gz"
 
 [ -z "${MONGO_URI}" ] && { echo "Please specify MONGO_URI" > /dev/stderr; exit 1;}
+[ -z "${S3_BUCKET}" ] && { echo "Please specify S3_BUCKET" > /dev/stderr; exit 1;}
+[ -z "${S3_PATH}" ] && { echo "Please specify S3_PATH" > /dev/stderr; exit 1;}
 
 echo "Backing up ${MONGO_URI}"
 
-mongodump -v --uri ${MONGO_URI} --archive=./tmp/${BACKUP_NAME} --gzip
+mongodump -v --uri ${MONGO_URI} --archive --gzip | aws s3 cp - s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}
 
 echo "Backup up FINISHED"
